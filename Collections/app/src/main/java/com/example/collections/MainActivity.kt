b3ff1d8f62bfa326bd11e81d5b0afb78d8d16955
David Barcenas <daveepro@outlook.com>
2022-04-28 14:07:25 -0500

    show scrollable list

--- Collections/app/src/main/java/com/example/collections/MainActivity.kt
+++ Collections/app/src/main/java/com/example/collections/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.collections
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.collections.adapter.ItemAdapter
+import com.example.collections.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadPhrases()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
