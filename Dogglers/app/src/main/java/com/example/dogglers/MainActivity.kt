018cb19bda27ef1b8888a3ee67e1a6b0e38faa5d
David Barcenas <daveepro@outlook.com>
2022-04-28 21:16:41 -0500

    add horizontal activity

--- Dogglers/app/src/main/java/com/example/dogglers/MainActivity.kt
+++ Dogglers/app/src/main/java/com/example/dogglers/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.dogglers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dogglers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.horizontalBtn.setOnClickListener { launchHorizontal() }
+    }
+
+    private fun launchHorizontal() {
+        listIntent = Intent(this, HorizontalListActivity::class.java)
+        startActivity(listIntent)
     }
 }
