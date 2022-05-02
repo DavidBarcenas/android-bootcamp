bdcf7ab72d68fe4ceb2f7932ec01e1749c29bacb
David Barcenas <daveepro@outlook.com>
2022-05-02 11:39:36 -0500

    create word adapter

--- Words/app/src/main/java/com/example/words/MainActivity.kt
+++ Words/app/src/main/java/com/example/words/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.words
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.words.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        // recyclerView.adapter = LetterAdapter()
     }
 }
