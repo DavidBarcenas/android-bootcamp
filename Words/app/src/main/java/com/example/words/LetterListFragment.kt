f3a8f805f9013d17859a16869a69877357325e17
David Barcenas <daveepro@outlook.com>
2022-05-06 10:27:54 -0500

    change activities to fragments

---
+++ Words/app/src/main/java/com/example/words/LetterListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.words
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LetterListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LetterListFragment : Fragment() {
+
+}
---
+++ Words/app/src/main/java/com/example/words/WordListFragment.kt
@@ -0,0 +1,11 @@
+package com.example.words
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WordListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WordListFragment : Fragment() {
+}
