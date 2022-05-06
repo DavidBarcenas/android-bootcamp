eaa583ba0933987da5d84ac04d14a2f403e0a715
David Barcenas <daveepro@outlook.com>
2022-05-06 17:55:57 -0500

    migrate DetailActivity to WordListFragment

--- Words/app/src/main/java/com/example/words/WordAdapter.kt
+++ Words/app/src/main/java/com/example/words/WordAdapter.kt
@@ -61,7 +61,7 @@ class WordAdapter(private val letterId: String, context: Context) :
         holder.button.text = item
 
         holder.button.setOnClickListener {
-            val queryUrl: Uri = Uri.parse("${DetailActivity.SEARCH_PREFIX}${item}")
+            val queryUrl: Uri = Uri.parse("${WordListFragment.SEARCH_PREFIX}${item}")
             val intent = Intent(Intent.ACTION_VIEW, queryUrl)
             context.startActivity(intent)
         }
--- Words/app/src/main/java/com/example/words/WordListFragment.kt
+++ Words/app/src/main/java/com/example/words/WordListFragment.kt
@@ -1,6 +1,14 @@
 package com.example.words
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.words.databinding.FragmentWordListBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -8,4 +16,42 @@ import androidx.fragment.app.Fragment
  * create an instance of this fragment.
  */
 class WordListFragment : Fragment() {
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    val letterId = activity?.intent?.extras?.getString(DetailActivity.LETTER).toString()
+
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter =  WordAdapter(letterId, this.requireContext())
+
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        title = getString(R.string.detail_prefix) + " " + letterId
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
