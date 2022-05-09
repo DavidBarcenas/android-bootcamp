5f20dfae59158368e7da3504bb83eb3969d7d939
David Barcenas <daveepro@outlook.com>
2022-05-09 17:56:54 -0500

    create game fragment layout

---
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/game/GameFragment.kt
@@ -0,0 +1,29 @@
+package com.example.unscramble.ui.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.unscramble.databinding.GameFragmentBinding
+
+class GameFragment : Fragment() {
+    private var score = 0
+    private var currentWordCount = 0
+    private var currentScrambleWord = "test"
+
+    // Binding object instance with access to the views in the game_fragment.xml layout
+    private lateinit var binding: GameFragmentBinding
+
+    // Create a ViewModel the first time the fragment is created.
+    // If the fragment is re-created, it receives the same GameViewModel instance created by the
+    // first fragment
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = GameFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
