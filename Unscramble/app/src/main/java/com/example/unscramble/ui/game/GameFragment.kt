138cd2d58900b9243422948e580f4d7f854dc143
David Barcenas <daveepro@outlook.com>
2022-05-14 18:39:30 -0500

    add functionality to skip button

--- Unscramble/app/src/main/java/com/example/unscramble/ui/game/GameFragment.kt
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/game/GameFragment.kt
@@ -70,6 +70,12 @@ class GameFragment : Fragment() {
      * Increases the word count.
      */
     private fun onSkipWord() {
+        if(viewModel.nextWord()) {
+            setErrorTextField(false)
+            updateNextWordOnScreen()
+        } else {
+            showFinalScoreDialog()
+        }
     }
 
     /*
